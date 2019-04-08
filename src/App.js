634e340215b4bdedb9b118b900c9fe6384a416f4
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-04-08 14:57:00 +0300

    basic stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import React, { Component } from 'react';
 import './App.css';
+import events from './Events.json';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLevel: null
+    }
+  }
+
+  renderTags(tags) {
+    return tags.map(tag => {
+      return (
+        <div>
+          <p>{tag}</p>
+        </div>
+      )
+    })
+  }
+
+  renderEvents() {
+
+    const filtered = events.filter(event => {
+      if (event.level === this.state.selectedLevel) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    return filtered.map(event => {
+      return (
+        <div className="Event">
+          <h1 className="Event-title">{event.name} by {event.host}</h1>
+          <p className="Event-time">Kello: {event.start_time} - {event.end_time}</p>
+          {this.renderTags(event.tags)}
+        </div>
+      )
+    })
+  }
+
+  renderFilters(levels) {
+    return levels.map(level => {
+      return (
+        <button onClick={() => this.setState({ selectedLevel: level })}>{level}</button>
+      )
+    })
+  }
+
   render() {
+    const mapped = events.map(event => event.name);
+
+    const skillLevels = [];
+
+    events.forEach(event => {
+      if (skillLevels.indexOf(event.level) === -1) {
+        skillLevels.push(event.level);
+      }
+    })
+
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h1 className="Title">Työpajatyökalu</h1>
+          <p>Valittu taso:  {this.state.selectedLevel}</p>
+          <div className="SkillLevelFilters">
+            {this.renderFilters(skillLevels)}
+          </div>
+          {this.renderEvents()}
+        </div>
       </div>
     );
   }
